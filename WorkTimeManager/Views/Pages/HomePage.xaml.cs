41cca2434b0eee743115df92d2166597b1d543b2
rufar <rufar@LAPTOP-ISCUOO7P>
2021-03-05 20:37:08 +0300

    Добавил страницы

---
+++ WorkTimeManager/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorkTimeManager.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        private readonly Func<object, bool> navigator;
+
+        public HomePage(Func<object, bool> navigator)
+        {
+            InitializeComponent();
+            this.navigator = navigator;
+        }
+
+        private void NavigateToEmployees(object sender, MouseButtonEventArgs e)
+        {
+            navigator.Invoke(new EmployeesPage());
+        }
+
+        private void NavigateToWorkTime(object sender, MouseButtonEventArgs e)
+        {
+            navigator.Invoke(new WorkTimePage());
+        }
+    }
+}
--- WorkTimeManager/Views/Pages/WorkTimePage.xaml.cs
+++ WorkTimeManager/Views/Pages/WorkTimePage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,19 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WorkTimeManager.Views.Pages;
 
-namespace WorkTimeManager.Views
+namespace WorkTimeManager.Views.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WorkTimePage.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class WorkTimePage : Page
     {
-        public MainWindow()
+        public WorkTimePage()
         {
             InitializeComponent();
-            Navigate(new HomePage(Navigate));
         }
     }
 }
